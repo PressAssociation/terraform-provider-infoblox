d8ebbbd106db66914c3174d621e6ac84c8f5b3db
Marco Masetti <marco.masetti@sky.uk>
2017-09-21 13:57:06 +0100

    Added a check to slice values

--- infoblox/resource_adminuser_test.go
+++ infoblox/resource_adminuser_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/sky-uk/skyinfoblox/api/common/v261/model"
+	"log"
+	"strconv"
 	"testing"
 )
 
@@ -28,7 +30,7 @@ func TestAccAdminUserResource(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "name", recordUserName),
 					resource.TestCheckResourceAttr(resourceName, "comment", "this is a comment"),
 					resource.TestCheckResourceAttr(resourceName, "email", "exampleuser@domain.internal.com"),
-					//resource.TestCheckResourceAttr(resourceName, "admin_groups", []string{"APP-OVP-INFOBLOX-READONLY"}),
+					testCheckResourceStringSlice(resourceName, "admin_groups", []string{"APP-OVP-INFOBLOX-READONLY"}),
 				),
 			}, {
 				Config: testAccResourceAdminUserNameUpdateTemplate(recordUserName),
@@ -37,12 +39,53 @@ func TestAccAdminUserResource(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "name", recordUserName),
 					resource.TestCheckResourceAttr(resourceName, "comment", "this is a comment updated"),
 					resource.TestCheckResourceAttr(resourceName, "email", "user@domain.internal.com"),
-					//resource.TestCheckResourceAttr(resourceName, "admin_groups", []string{"APP-OVP-INFOBLOX-READONLY"}),
+					testCheckResourceStringSlice(resourceName, "admin_groups", []string{"APP-OVP-INFOBLOX-READONLY"}),
 				),
 			},
 		},
 	})
 
+}
+
+//testCheckResourceStringSlice - test that a slice has the same content
+func testCheckResourceStringSlice(name, key string, expected []string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		is, err := primaryInstanceState(s, name)
+		if err != nil {
+			return err
+		}
+
+		for k := range is.Attributes {
+			log.Printf("attr[%s]:%+v\n", k, is.Attributes[k])
+		}
+
+		if sliceLength, found := is.Attributes[key+".#"]; found {
+			i, err := strconv.Atoi(sliceLength)
+			if err != nil {
+				return err
+			}
+			if i != len(expected) {
+				return fmt.Errorf("Attr %s expected lenght [ %d ] differs from actual [ %s ]\n", key, len(expected), sliceLength)
+			}
+
+		} else {
+			return fmt.Errorf("Error getting attr %s length", key)
+		}
+
+		for index := 0; index < len(expected); index++ {
+			indexKey := key + "." + strconv.Itoa(index)
+			log.Println("Looking for key: ", indexKey)
+			if actual, ok := is.Attributes[indexKey]; ok {
+				if actual != expected[index] {
+					return fmt.Errorf("%s[%d] expected this:\n%s\nActual:\n%s\n", key, index, expected[index], actual)
+				}
+			} else {
+				return fmt.Errorf("Attribute %s not found in resource %s\n", key, name)
+			}
+		}
+		return nil
+	}
 }
 
 func testAccResourceAdminUserExists(key, value string) resource.TestCheckFunc {
@@ -71,4 +114,20 @@ func testAccResourceAdminUserNameUpdateTemplate(username string) string {
 		password = "c0a6264f0f128d94cd8ef26652e7d9fd"
 	}
 	`, username)
+}
+
+// primaryInstanceState returns the primary instance state for the given resource name.
+func primaryInstanceState(s *terraform.State, name string) (*terraform.InstanceState, error) {
+	ms := s.RootModule()
+	rs, ok := ms.Resources[name]
+	if !ok {
+		return nil, fmt.Errorf("Not found: %s", name)
+	}
+
+	is := rs.Primary
+	if is == nil {
+		return nil, fmt.Errorf("No primary instance: %s", name)
+	}
+
+	return is, nil
 }
